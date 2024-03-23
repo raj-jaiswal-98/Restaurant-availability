9783846f7e8af690917b090c52320438c08470e8
raj-jaiswal-98 <rajwashere98@gmail.com>
2024-03-23 15:14:25 +0530

    added datasets given

--- orm_scripts.py
+++ orm_scripts.py
@@ -8,7 +8,7 @@ from csv import DictReader
 def run():
     #timezones
     Store_tz.objects.all().delete()
-    with open('store timezones.csv', 'r') as csvfile:
+    with open('data/store timezones.csv', 'r') as csvfile:
         reader = DictReader(csvfile)
         for row in reader:
             Store_tz.objects.create(store_id = int(row['store_id']), timezone = row['timezone_str'])
@@ -16,7 +16,7 @@ def run():
 
     #open hours
     Store.objects.all().delete()
-    with open('Menu hours.csv', 'r') as csvfile:
+    with open('data/Menu hours.csv', 'r') as csvfile:
         reader = DictReader(csvfile)
         for row in reader:
             Store.objects.create(store_id = int(row['store_id']), day_of_week = int(row['day']), start_time_local = time.fromisoformat(row['start_time_local']), end_time_local = time.fromisoformat(row['end_time_local']))
@@ -25,7 +25,7 @@ def run():
     print("Menu hours  done!!")
     #status
     # Store_status.objects.all().delete()
-    with open('store status.csv', 'r') as csvfile:
+    with open('data/store status.csv', 'r') as csvfile:
         reader = DictReader(csvfile)
         for row in reader:
             Store_status.objects.create(store_id = int(row['store_id']), timestamp_utc = dt.fromisoformat(f"{row['timestamp_utc'][0:-4]}+00:00"), status = (True if row['status'] == 'active' else False))
